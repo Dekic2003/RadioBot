02c813d632d37f4d727371879b04b5377711728c
Dekic2003 <Dekic2003@users.noreply.github.com>
2020-05-29 00:54:44 +0200

    :rocket: Deploying multiple naxi

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const client = new Discord.Client();
 const dotenv= require('dotenv');
 dotenv.config();
 const prefix=process.env.PREFIX;
+const naxi = [
+    'house', 'evergreen', 'cafe', 'classic', '80s', 'kids', 'gold', 'clubbing','','',
+    'love', 'dance', 'rnb', '', '', '', 'boem', 'jazz',
+    'rock', '', '', 'fresh', 'mix', 'latino', 'exyu', 'lounge', '','hype'
+];
 
 client.once('ready', () => {
     console.log('Moze!');
@@ -14,8 +19,20 @@ client.on('message', async (message) => {
 
     if (message.content.startsWith(`${prefix}naxi`)) {
         const connection = await message.member.voice.channel.join();
-        connection.play('http://naxi128.streaming.rs:9150/');
-    }
+        const radio_index = prefix.length + 'naxi '.length;
+        const radio_name = message.content.substring(radio_index);
+
+                connection.play('http://naxidigital-house128.streaming.rs:8000/;*.mp3');
+
+            if ( radio_name!=='' && naxi.includes(radio_name)) {
+                const radio_number = ('0' + naxi.indexOf(radio_name)).slice(-2);
+                let link =`http://naxidigital-${radio_name}128.streaming.rs:8${radio_number}0/;*.mp3`;
+                connection.play(link);
+                console.log(link);
+            }
+
+
+        }
     if (message.content.startsWith(`${prefix}rsg`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://195.201.112.14:9000/;stream');
