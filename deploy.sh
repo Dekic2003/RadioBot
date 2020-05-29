df6edfc489cbdcda1c71bab98661bbeedc7d1367
Dekic2003 <Dekic2003@users.noreply.github.com>
2020-05-29 12:18:47 +0200

    Provjera

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
+
 git pull
 npm install
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker build --rm -f Dockerfile -t radio:latest .
-docker run --rm -d -p 80:80 radio:latest
+docker run --rm -d -p 3000:3000 radio:latest
