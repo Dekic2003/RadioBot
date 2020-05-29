bd7febe36fce1c240b3e2c4c01160949bc4f7527
Dekic2003 <Dekic2003@users.noreply.github.com>
2020-05-29 11:55:58 +0200

    :seedling: Adding .sh file

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+###########################
+#                         #
+#      deploy script      #
+#        By A.Ajdin       #
+###########################
+
+
+#Pull changes
+git pull
+
+#Install dependencies
+npm install
+
+#Stop all docker container and delete them
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+#Build docker image
+docker build --rm -f Dockerfile -t radio:latest .
+
+#Run docker image
+docker run --rm -d -p 80:80 radio:latest
