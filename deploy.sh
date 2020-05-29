4308d9bed4092c4d19673931b700c39bffb283c7
Dekic2003 <Dekic2003@users.noreply.github.com>
2020-05-29 16:03:10 +0200

    :sparkles:Added playing

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-
-git pull
-npm install
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker build --rm -f Dockerfile -t radio:latest .
-docker run --rm -d -p 3000:3000 radio:latest
