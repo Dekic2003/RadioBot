b8de6f135ede18bc7046fa56b259d4f1dde724f5
Dekic2003 <Dekic2003@users.noreply.github.com>
2020-06-06 23:07:58 +0200

    :zap: Reconnect added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const prefix=process.env.PREFIX;
 
 let Currently='';
+let url='';
 const naxi = [
     'house', 'evergreen', 'cafe', 'classic', '80s', 'kids', 'gold', 'clubbing','','',
     'love', 'dance', 'rnb', '', '', '', 'boem', 'jazz',
@@ -12,8 +13,9 @@ const naxi = [
 ];
 let list='```*naxi - Naxi Radio\n*rsg - RSG Radio\n*orv - ORV Radio\n*feral - Feral Radio\n*glasDrine - Glas Drine\n*kameleon - Kameleon\n*kalman - Kalman\n*antenaZg - Antena Zagreb\n*otvorenMreza - Otvorena Mreza ```';
 let contact='```ORV - Viber:063 422 126\nKalman - 033 772 900 il 033 712 901```';
-function Playing(station , message){
+function Playing(station , message,link){
     Currently=station;
+    url=link;
     if(station==='ORV'){
         message.channel.send(`Svira ${station}\nKontakt Viber:063 422 126 `).then(msg=>{
             msg.delete({ timeout:100000});
@@ -30,6 +32,10 @@ function Playing(station , message){
         msg.delete({ timeout:100000});
 
     })}
+}
+function refresh(message){
+    const connection = message.member.voice.channel.join();
+    connection.play(url);
 }
 
 client.once('ready', () => {
@@ -52,14 +58,14 @@ client.on('message', async (message) => {
             if ( radio_name!=='' && naxi.includes(radio_name)) {
                 const radio_number = ('0' + naxi.indexOf(radio_name)).slice(-2);
                 let link =`http://naxidigital-${radio_name}128.streaming.rs:8${radio_number}0/;*.mp3`;
-                let rest=radio_name.toUpperCase()
-                Playing(`NAXI ${rest}`,message);
+                let rest=radio_name.toUpperCase();
+                Playing(`NAXI ${rest}`,message,`http://naxidigital-${radio_name}128.streaming.rs:8${radio_number}0/;*.mp3`);
 
                 connection.play(link);
             }
             else{
                 connection.play('http://naxidigital-house128.streaming.rs:8000/;*.mp3');
-                Playing('NAXI',message);
+                Playing('NAXI',message,'http://naxidigital-house128.streaming.rs:8000/;*.mp3');
             }
 
 
@@ -67,45 +73,45 @@ client.on('message', async (message) => {
     if (message.content.startsWith(`${prefix}rsg`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://195.201.112.14:9000/;stream');
-        Playing('RSG',message);
+        Playing('RSG',message,'http://195.201.112.14:9000/;stream');
     }
     if (message.content.startsWith(`${prefix}kameleon`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://188.40.62.20:8006/;stream.mp3');
-        Playing('Kameleon',message);
+        Playing('Kameleon',message,'http://188.40.62.20:8006/;stream.mp3');
     }
     if (message.content.startsWith(`${prefix}antenaZg`)) {
     const connection = await message.member.voice.channel.join();
     connection.play('http://live.antenazagreb.hr:8002/;*.mp3');
-        Playing('Antena Zagreb',message);
+        Playing('Antena Zagreb',message,'http://live.antenazagreb.hr:8002/;*.mp3');
     }
     if (message.content.startsWith(`${prefix}feral`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://185.50.56.3:80/;*.mp3');
-        Playing('Feral',message);
+        Playing('Feral',message,'http://185.50.56.3:80/;*.mp3');
     }
     if (message.content.startsWith(`${prefix}kalman`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://163.172.213.155:8203/;*.mp3');
-        Playing('Kalman',message);
+        Playing('Kalman',message,'http://163.172.213.155:8203/;*.mp3');
     }
     if (message.content.startsWith(`${prefix}orv`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://77.74.231.19:7109/stream');
-        Playing('ORV',message);
+        Playing('ORV',message,'http://77.74.231.19:7109/stream');
 
 
     }
     if (message.content.startsWith(`${prefix}otvorenaMreza`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://s8.iqstreaming.com:8050/stream;');
-        Playing('Otvorena Mreza',message);
+        Playing('Otvorena Mreza',message,'http://s8.iqstreaming.com:8050/stream;');
 
     }
     if (message.content.startsWith(`${prefix}glasDrine`)) {
         const connection = await message.member.voice.channel.join();
         connection.play('http://77.74.231.21:8028/;*.mp3');
-        Playing('Glas Drine',message);
+        Playing('Glas Drine',message,'http://77.74.231.21:8028/;*.mp3');
 
 
     }
@@ -116,6 +122,15 @@ client.on('message', async (message) => {
     if(message.content.startsWith(`${prefix}kontakt`)){
         message.channel.send(contact)
     }
+    if(message.content.startsWith(`${prefix}refresh`)){
+        message.member.voice.channel.leave();
+        setTimeout(function(){
+          message.member.voice.channel.join();
+          Currently='';
+        },1000)
+
+    }
+
     client.user.setActivity(`${Currently}`,{type:'PLAYING'});
 
 
